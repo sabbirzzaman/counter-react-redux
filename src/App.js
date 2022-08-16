23221baf2b33eeac5c2ad406636d696ff7820030
sabbirzzaman <sztonmoy7@gmail.com>
2022-08-16 21:28:07 +0600

    reudx setuped

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
-import './App.css';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 function App() {
-  return (
-    <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
-            <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
-                Simple Counter Application
-            </h1>
+    return (
+        <Provider store={store}>
+            <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+                <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
+                    Simple Counter Application
+                </h1>
 
-            <div class="max-w-md mx-auto mt-10 space-y-5">
-                <div
-                    class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
-                >
-                    <div class="text-2xl font-semibold" id="count"></div>
-                    <div class="flex space-x-3">
-                        <button
-                            class="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-                            id="increase"
-                        >
-                            Increment
-                        </button>
-                        <button
-                            class="bg-red-400 text-white px-3 py-2 rounded shadow"
-                            id="decrease"
-                        >
-                            Decrement
-                        </button>
+                <div className="max-w-md mx-auto mt-10 space-y-5">
+                    <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
+                        <div className="text-2xl font-semibold">0</div>
+                        <div className="flex space-x-3">
+                            <button className="bg-indigo-400 text-white px-3 py-2 rounded shadow">
+                                Increment
+                            </button>
+                            <button className="bg-red-400 text-white px-3 py-2 rounded shadow">
+                                Decrement
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-  );
+        </Provider>
+    );
 }
 
 export default App;
